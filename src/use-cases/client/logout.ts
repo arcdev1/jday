fe41b458242b71760b80412435ed8bf63cf1b9fc
Bill Sourour <bill.sourour@arcnovus.com>
2023-10-07 18:11:51 -0400

    handle response more gracefully

--- src/use-cases/client/logout.ts
+++ src/use-cases/client/logout.ts
@@ -2,5 +2,8 @@ import { SESSION_KEY } from "~/models/session";
 
 export async function logout() {
   sessionStorage.removeItem(SESSION_KEY);
-  fetch("/api/session", { method: "DELETE" });
+  const response = await fetch("/api/session", { method: "DELETE" });
+  if (!response.ok) {
+    throw new Error("Failed to logout.");
+  }
 }
